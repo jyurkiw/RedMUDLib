12439a3bfefa376aa55f3278c5d2b055b0527990
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-10-23 23:59:52 -0700

    Fixed async race conditions with addRoom. Wrote test coverage for create and read. Wrote API methods for create, read, and update.

--- test/room-test.js
+++ test/room-test.js
@@ -24,12 +24,6 @@ var goblinValleyArea = {
     size: 0
 };
 
-var westernOverlookAdd = {
-    areacode: koboldValleyArea.areacode,
-    name: 'Western Overlook',
-    description: 'A short cliff overlooks a small, fertile valley. You can see scores of Kobolds milling about doing whatever it is Kobolds do.'
-};
-
 var westernOverlook = {
     areacode: koboldValleyArea.areacode,
     roomnumber: 1,
@@ -37,31 +31,19 @@ var westernOverlook = {
     description: 'A short cliff overlooks a small, fertile valley. You can see scores of Kobolds milling about doing whatever it is Kobolds do.'
 };
 
-var goblinCaveEntranceAdd = {
-    areacode: goblinValleyArea.areacode,
-    name: 'Cave Entrance',
-    description: 'The opening to this dank cave reeks of Goblin.'
-};
-
 var goblinCaveEntrance = {
     areacode: goblinValleyArea.areacode,
     roomnumber: 1,
     name: 'Cave Entrance',
     description: 'The opening to this dank cave reeks of Goblin.'
-}
-
-var goblinCaveTunnelAdd = {
-    areacode: goblinValleyArea.areacode,
-    name: 'Narrow Corridor',
-    description: 'The cave stretches on into the darkness. '
-}
+};
 
 var goblinCaveTunnel = {
     areacode: goblinValleyArea.areacode,
     roomnumber: 2,
     name: 'Narrow Corridor',
     description: 'The cave stretches on into the darkness. '
-}
+};
 
 var westernOverlookUpdate = {
     name: 'Western Overlook',
@@ -72,58 +54,71 @@ var westernOverlookUpdated = {
     areacode: 'KDV',
     roomnumber: 1,
     name: 'Western Overlook',
-    description: 'A short cliff overlooks a small, fertile valley. You can see scores of Kobolds milling about doing whatever it is Kobolds do. A hole in the wester rockface opens into a dark cave that reeks of Goblin.'
+    description: 'A short cliff overlooks a small, fertile valley. You can see scores of Kobolds milling about doing whatever it is Kobolds do. A hole in the western rockface opens into a dark cave that reeks of Goblin.'
 };
-
-// Setup
-beforeEach(function(done) {
-    client.flushall();
-    lib.setArea(koboldValleyArea.areacode, koboldValleyArea);
-    lib.setArea(goblinValleyArea.areacode, goblinValleyArea);
-    lib.addRoom(koboldValleyArea.areacode, westernOverlookAdd);
-    done();
-});
-
-after(function(done) {
-    client.flushall();
-    done();
-});
 
 // Tests
 describe('The room-lib module', function() {
+    // Setup
+    beforeEach(function(done) {
+        client.flushall();
+        lib.setArea(koboldValleyArea.areacode, koboldValleyArea);
+        lib.setArea(goblinValleyArea.areacode, goblinValleyArea);
+
+        lib.reserveRoomNumber(koboldValleyArea.areacode, function(roomNumber) {
+            westernOverlook.roomnumber = roomNumber;
+            lib.setRoom(koboldValleyArea.areacode, roomNumber, westernOverlook);
+            done();
+        });
+    });
+
+    after(function(done) {
+        client.flushall();
+        done();
+    });
+
     // C
     describe('Create a new room without exits', function() {
-        it('Checks setting of all data pieces with basic area code', function(done) {
-            lib.addRoom(goblinCaveEntranceAdd.areacode, goblinCaveEntranceAdd, function(fullRoomData, newRoomNumber) {
-                expect(newRoomNumber).to.equal(goblinCaveEntrance.roomnumber);
-                expect(fullRoomData).to.deep.equal(goblinCaveEntrance);
-                done();
+        it('Create goblin cave entrance through reserveRoomNumber and setRoom', function(done) {
+            lib.reserveRoomNumber(goblinValleyArea.areacode, function(roomNumber) {
+                goblinCaveEntrance.roomnumber = roomNumber;
+                lib.setRoom(goblinValleyArea.areacode, roomNumber, goblinCaveEntrance, function() {
+                    client.hgetall(codeutil.buildRoomCode(goblinCaveEntrance.areacode, roomNumber), function(err, res) {
+                        if (typeof(res.roomnumber) === 'string') {
+                            res.roomnumber = parseInt(res.roomnumber, 10);
+                        }
+                        expect(res).to.deep.equal(goblinCaveEntrance);
+                        done();
+                    });
+                });
             });
         });
 
-        it('Checks setting of all data pieces with area key', function(done) {
-            var areaCode = codeutil.buildAreaCode(goblinCaveEntranceAdd.areacode, goblinCaveEntranceAdd);
-
-            lib.addRoom(areaCode, goblinCaveEntranceAdd, function(fullRoomData, newRoomNumber) {
-                expect(newRoomNumber).to.equal(goblinCaveEntrance.roomnumber);
-                expect(fullRoomData).to.deep.equal(goblinCaveEntrance);
-                done();
+        it('Create goblin cave entrance through addRoom', function(done) {
+            lib.addRoom(goblinValleyArea.areacode, goblinCaveEntrance, function(roomNumber) {
+                client.hgetall(codeutil.buildRoomCode(goblinCaveEntrance.areacode, roomNumber), function(err, res) {
+                    if (typeof(res.roomnumber) === 'string') {
+                        res.roomnumber = parseInt(res.roomnumber, 10);
+                    }
+                    expect(res).to.deep.equal(goblinCaveEntrance);
+                    done();
+                });
             });
-        });
+        })
     });
 
     // R
     describe('Read rooms', function() {
-        it('Get the western overlook', function(done) {
-            lib.getRoom(westernOverlook.areacode, westernOverlook.roomnumber, function(room) {
-                expect(room).to.deep.equal(westernOverlook);
+        it('Read the western overlook.', function(done) {
+            lib.getRoom(westernOverlook.areacode, westernOverlook.roomnumber, function(roomData) {
+                expect(roomData).to.deep.equal(westernOverlook);
                 done();
             });
         });
     });
 
     // U
-    describe('Update a room data', function() {
+    describe('Update room data', function() {
 
     });
 
