fb3a347fb54c361b83061062ceb504f438ac350d
jeff yurkiw <jeff yurkiw>
2016-10-23 17:03:29 -0700

    Created getAreas test. Created getArea test. Created setArea test for editing. Created getAreas() method. Created getArea() method.
    Documented area-lib.js and codeutil.js to JSDoc spec.

--- codeutil.js
+++ codeutil.js
@@ -1,31 +1,76 @@
+/**
+ * Simple Code Utility library.
+ * "Code"s are the identifier codes the game uses to store data in the database.
+ * 
+ * @returns An access object.
+ */
 function CodeUtil() {
     var constants = require('./serverconstants.js');
 
+    /**
+     * Base code building function. Other functions call this.
+     * 
+     * @param {arguments:any} Passed arguments must be castable to string through a toString() function.
+     * @returns Joins all argument values with colons.
+     */
     function BuildCode() {
         var args = [];
         for (i = 0; i < arguments.length; i++)
-            args.push(arguments[i]);
+            args.push(arguments[i].toString());
         return args.join(":");
-    };
+    }
 
+    /**
+     * Builds an area code.
+     * Area codes are prefaced with the AREAS: key.
+     * If the passed area code already begins with AREAS:, no further building will occur.
+     * 
+     * @param {any} areaCode The code to append to AREAS:
+     * @returns A fully-formatted area code.
+     */
     function BuildAreaCode(areaCode) {
-        return BuildCode(constants.AREAS_KEY, areaCode);
-    };
+        // If areaCode starts with 'AREAS:', just return the areaCode
+        if (areaCode.search(constants.AREAS_KEY + ':') === 0) {
+            return areaCode;
+        } else {
+            return BuildCode(constants.AREAS_KEY, areaCode);
+        }
+    }
 
+    /**
+     * Builds a room code.
+     * 
+     * @param {any} areaCode The room's area.
+     * @param {number} roomNumber The room's number.
+     * @returns A fully-formed room code.
+     */
     function BuildRoomCode(areaCode, roomNumber) {
+        if (typeof(roomNumber) != 'number') {
+            throw "number expected";
+        }
         return BuildCode(constants.ROOMS_KEY, areaCode, roomNumber);
-    };
+    }
 
+    /**
+     * Builds a room exits code.
+     * 
+     * @param {any} areaCode The room's area.
+     * @param {number} roomNumber The room's number.
+     * @returns A fully-formed room exits code.
+     */
     function BuildRoomExitsCode(areaCode, roomNumber) {
+        if (typeof(roomNumber) != 'number') {
+            throw "number expected";
+        }
         return BuildCode(constants.ROOMS_KEY, areaCode, roomNumber, constants.ROOMS_EXIT_KEY);
-    };
+    }
 
     return {
         buildCode: BuildCode,
         buildAreaCode: BuildAreaCode,
         buildRoomCode: BuildRoomCode,
         buildRoomExitsCode: BuildRoomExitsCode
-    }
+    };
 }
 
 module.exports = CodeUtil();
--- test/area-test.js
+++ test/area-test.js
@@ -15,17 +15,49 @@ var testArea = {
     name: "Kobold Valley",
     description: "A valley filled with dangerous Kobolds.",
     size: 0
-}
+};
+
+var testArea2 = {
+    areacode: "GCV",
+    name: "Goblin Cave",
+    description: "A cave filled with goblins.",
+    size: 0
+};
+
+var testAreaUpdate = {
+    name: "Kobold Death Valley",
+    description: "A hot, dry valley filled with undead Kobolds."
+};
+
+var testAreaUpdated = {
+    areacode: "KDV",
+    name: "Kobold Death Valley",
+    description: "A hot, dry valley filled with undead Kobolds.",
+    size: 0
+};
 
 var hashKey = codeutil.buildCode(constants.AREAS_KEY, testArea.areacode);
 
+// Setup
+beforeEach(function(done) {
+    client.flushall();
+    lib.setArea(testArea.areacode, testArea);
+    done();
+});
+
+after(function(done) {
+    client.flushall();
+    done();
+});
+
 // Tests
 describe('The area-lib module', function() {
+    // C
     describe('Create a new area', function() {
         it('Checks setting of both data pieces', function(done) {
-            lib.setArea(testArea.areacode, testArea);
+            lib.setArea(testArea2.areacode, testArea2);
 
-            assert(client.sismember(constants.AREAS_KEY, testArea.areacode, function(err, res) {
+            assert(client.sismember(constants.AREAS_KEY, testArea2.areacode, function(err, res) {
                 expect(res).to.equal(1);
                 done();
             }));
@@ -37,8 +69,51 @@ describe('The area-lib module', function() {
                 done();
             }));
         });
+
+        it('areaCode and areaData.areacode need to match.', function() {
+            expect(lib.setArea('a', testArea)).to.equal(false);
+        });
     });
 
+    // R
+    describe('Read area list', function() {
+        lib.setArea(testArea2.areacode, testArea2);
+        var areaArray = [testArea.areacode];
+
+        it('Retrieve list of all areas', function(done) {
+            lib.getAreas(function(res) {
+                expect(res).to.have.length(areaArray.length);
+                for (var idx in areaArray) {
+                    expect(res).to.contain(areaArray[idx]);
+                }
+                done();
+            });
+        });
+    });
+
+    describe('Read one area', function() {
+        var areaKey = codeutil.buildAreaCode(testArea.areacode);
+        it('Read data for one area', function(done) {
+            lib.getArea(areaKey, function(res) {
+                expect(res).to.deep.equal(testArea);
+                done();
+            });
+        });
+    });
+
+    // U
+    describe('Update one area', function() {
+        it('Update data for one area.', function(done) {
+            lib.setArea(testArea.areacode, testAreaUpdate);
+
+            lib.getArea(testArea.areacode, function(res) {
+                expect(res).to.deep.equal(testAreaUpdated);
+                done();
+            });
+        });
+    });
+
+    // D
     describe('Delete an existing area', function() {
         it('Checks delete of both data pieces', function(done) {
             lib.deleteArea(testArea.areacode);
