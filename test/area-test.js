ebed5c71a5fd0f5a994a01c46c6988ec5fd424b1
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-11-02 17:54:04 -0700

    Changed the structure of the mud-lib object to avoid name collissions.

--- test/area-test.js
+++ test/area-test.js
@@ -10,39 +10,39 @@ var client = redis.createClient();
 
 var lib = require('../mud-lib')(client);
 
-var testArea = {
+var koboldValleyArea = {
     areacode: "KDV",
     name: "Kobold Valley",
     description: "A valley filled with dangerous Kobolds.",
     size: 0
 };
 
-var testArea2 = {
+var goblinCaveArea = {
     areacode: "GCV",
     name: "Goblin Cave",
     description: "A cave filled with goblins.",
     size: 0
 };
 
-var testArea3 = {
+var goblinCaveNoSize = {
     areacode: "GCV",
     name: "Goblin Cave",
     description: "A cave filled with goblins."
 };
 
-var testAreaUpdate = {
+var koboldValleyUpdate = {
     name: "Kobold Death Valley",
     description: "A hot, dry valley filled with undead Kobolds."
 };
 
-var testAreaUpdated = {
+var koboldAreaUpdated = {
     areacode: "KDV",
     name: "Kobold Death Valley",
     description: "A hot, dry valley filled with undead Kobolds.",
     size: 0
 };
 
-var hashKey = codeutil.buildCode(constants.AREAS_KEY, testArea.areacode);
+var hashKey = codeutil.buildCode(constants.AREAS_KEY, koboldValleyArea.areacode);
 
 // Tests
 describe('The area-lib module', function() {
@@ -60,35 +60,35 @@ describe('The area-lib module', function() {
     // C
     describe('Create a new area', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
         it('Checks setting of both data pieces', function(done) {
-            lib.createArea(testArea2.areacode, testArea2, function(success) {
+            lib.area.createArea(goblinCaveArea.areacode, goblinCaveArea, function(success) {
                 client.multi()
-                    .sismember(constants.AREAS_KEY, testArea2.areacode)
+                    .sismember(constants.AREAS_KEY, goblinCaveArea.areacode)
                     .hmget(hashKey, 'areacode')
                     .exec(function(err, res) {
                         expect(res[0]).to.equal(1);
                         expect(res[1]).to.be.a('Array');
                         expect(res[1]).to.have.length(1);
-                        expect(res[1]).to.deep.equal([testArea.areacode]);
+                        expect(res[1]).to.deep.equal([koboldValleyArea.areacode]);
                         done();
                     });
             });
         });
 
         it('areaCode and areaData.areacode need to match.', function() {
-            lib.createArea('a', testArea, function(success) {
+            lib.area.createArea('a', koboldValleyArea, function(success) {
                 expect(success).to.equal(false);
             });
         });
 
         it('Create data for area with undefined size and verify size = 0', function(done) {
-            var test3Key = codeutil.buildCode(constants.AREAS_KEY, testArea3.areacode);
-            lib.createArea(testArea3.areacode, testArea3, function(success) {
+            var test3Key = codeutil.buildCode(constants.AREAS_KEY, goblinCaveNoSize.areacode);
+            lib.area.createArea(goblinCaveNoSize.areacode, goblinCaveNoSize, function(success) {
                 client.hmget(test3Key, 'size', function(err, res) {
                     expect(res).to.have.length(1);
                     expect(parseInt(res[0], 10)).to.equal(0);
@@ -101,15 +101,15 @@ describe('The area-lib module', function() {
     // R
     describe('Read area list', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
-        var areaArray = [testArea.areacode];
+        var areaArray = [koboldValleyArea.areacode];
 
         it('Retrieve list of all areas', function(done) {
-            lib.getAreas(function(res) {
+            lib.area.getAreas(function(res) {
                 expect(res).to.have.length(areaArray.length);
                 for (var idx in areaArray) {
                     expect(res).to.contain(areaArray[idx]);
@@ -121,24 +121,24 @@ describe('The area-lib module', function() {
 
     describe('Read one area', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
-        var areaKey = codeutil.buildAreaCode(testArea.areacode);
+        var areaKey = codeutil.buildAreaCode(koboldValleyArea.areacode);
         it('Read data for one area', function(done) {
-            lib.getArea(areaKey, function(res) {
-                expect(res).to.deep.equal(testArea);
+            lib.area.getArea(areaKey, function(res) {
+                expect(res).to.deep.equal(koboldValleyArea);
                 done();
             });
         });
 
         it('Read data for area with size > 0 and verify size', function(done) {
-            var areaKey = codeutil.buildAreaCode(testArea.areacode);
+            var areaKey = codeutil.buildAreaCode(koboldValleyArea.areacode);
 
             client.hincrby(areaKey, 'size', 5, function(herr, hres) {
-                lib.getArea(testArea.areacode, function(res) {
+                lib.area.getArea(koboldValleyArea.areacode, function(res) {
                     expect(res.size).to.equal(5);
                     done();
                 });
@@ -148,20 +148,20 @@ describe('The area-lib module', function() {
 
     describe('Check area exists', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
         it('Area does exist.', function(done) {
-            lib.areaExists(testArea.areacode, function(exists) {
+            lib.area.areaExists(koboldValleyArea.areacode, function(exists) {
                 assert(exists);
                 done();
             });
         });
 
         it('Area does not exist.', function(done) {
-            lib.areaExists('XXX', function(exists) {
+            lib.area.areaExists('XXX', function(exists) {
                 assert(!exists);
                 done();
             });
@@ -171,14 +171,14 @@ describe('The area-lib module', function() {
     // U
     describe('Update one area', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
         it('Update data for one area.', function(done) {
-            lib.setArea(testArea.areacode, testAreaUpdate, function(area) {
-                expect(area).to.deep.equal(testAreaUpdated);
+            lib.area.setArea(koboldValleyArea.areacode, koboldValleyUpdate, function(area) {
+                expect(area).to.deep.equal(koboldAreaUpdated);
                 done();
             });
         });
@@ -186,7 +186,7 @@ describe('The area-lib module', function() {
 
     describe('Attempt to update a non-existant area', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
@@ -198,7 +198,7 @@ describe('The area-lib module', function() {
                 description: "A hot, dry valley filled with undead Kobolds."
             };
 
-            lib.setArea(newArea.areacode, newArea, function(err) {
+            lib.area.setArea(newArea.areacode, newArea, function(err) {
                 expect(err).to.not.equal(null);
                 expect(err).to.equal(constants.errors.UPDATE_AREACODE_NO_EXIST);
                 done();
@@ -210,15 +210,15 @@ describe('The area-lib module', function() {
     // D
     describe('Delete an existing area', function() {
         beforeEach(function(done) {
-            lib.createArea(testArea.areacode, testArea, function() {
+            lib.area.createArea(koboldValleyArea.areacode, koboldValleyArea, function() {
                 done();
             });
         });
 
         it('Checks delete of both data pieces', function(done) {
-            lib.deleteArea(testArea.areacode);
+            lib.area.deleteArea(koboldValleyArea.areacode);
 
-            assert(client.sismember(constants.AREAS_KEY, testArea.areacode, function(err, res) {
+            assert(client.sismember(constants.AREAS_KEY, koboldValleyArea.areacode, function(err, res) {
                 expect(res).to.equal(0);
                 done();
             }));
